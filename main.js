077b2b1fc3a46ccd67a0b9fe459b0416b7005590
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-02-11 20:13:34 +0100

    button gekoppeld aan countryinformationfunction

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const searchbutton = document.getElementById('searchbutton')
+const searchbutton = document.getElementById('searchbutton');
 
-async function getCountryInformation(){
+async function getCountryInformation(country) {
     // console.log("werk ik? ")
-
+    const belgie = "belgie"
+    const response = await axios.get("https://restcountries.eu/rest/v2/name/" + country +"?fullText=true");
+    console.log(response)
 }
 
+const belgie = "belgie"
+searchbutton.addEventListener("click", function(){
+    getCountryInformation(belgie)
+});
 
