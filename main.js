493f3fef925c58ce36a81b8b9210973559b4946c
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-02-11 20:45:46 +0100

    zoekfunctie gekoppeld en eerste variant van de string toegevoegd

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const searchbutton = document.getElementById('searchbutton');
 
 async function getCountryInformation(country) {
     // console.log("werk ik? ")
-    const belgie = "belgie"
     const response = await axios.get("https://restcountries.eu/rest/v2/name/" + country +"?fullText=true");
-    console.log(response)
+    console.log(response.data[0]);
+
+    console.log(response.data[0].name + " is situated in " + response.data[0].subregion + ". \n" +
+        "It has a population of " + response.data[0].population + " people");
 }
 
 const belgie = "belgie"
