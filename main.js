e111f34498cb266b3412d78261949d084b97c43c
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-02-13 11:01:10 +0100

    valutafunctie werkt

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const searchbutton = document.getElementById('searchbutton');
 
 async function getCountryInformation(country) {
-    // console.log("werk ik? ")
-    const response = await axios.get("https://restcountries.eu/rest/v2/name/" + country +"?fullText=true");
+    const response = await axios.get("https://restcountries.eu/rest/v2/name/" + country + "?fullText=true");
     console.log(response.data[0]);
-
     console.log(response.data[0].name + " is situated in " + response.data[0].subregion + ". \n" +
-        "It has a population of " + response.data[0].population + " people");
+        "It has a population of " + response.data[0].population + " people. \n" +
+        "The capital city is " + response.data[0].capital + " \n" +
+        getCurrencyInformation(response.data))
 }
 
-const belgie = "belgie"
-searchbutton.addEventListener("click", function(){
-    getCountryInformation(belgie)
+function getCurrencyInformation(country) {
+    console.log("hallo functie");
+    if (country[0].currencies.length < 2) {
+        return "and you can pay with " + country[0].currencies[0].name + "'s";
+    }
+    if (country[0].currencies.length >= 2) {
+        return "and you can pay with " + country[0].currencies[0].name + "'s and " + country[0].currencies[1].name;
+    }
+}
+
+
+const belgie = "belgie";
+searchbutton.addEventListener("click", function () {
+    getCountryInformation(belgie);
 });
 
+// getCurrencyInformation(belgie);
