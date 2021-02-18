bcee32e7b1414fbef49e9480ddcd2491e8872863
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-02-18 11:07:29 +0100

    spinner toegevoegd, styling afgerond, errormessage uitgebreid, bug met currencies verwijderd

--- main.js
+++ main.js
@@ -3,25 +3,37 @@ const countryInput = document.getElementById("countryInput");
 const container = document.getElementById("countryContainer");
 
 async function getCountryInformation(country) {
-   try {
-       const response = await axios.get("https://restcountries.eu/rest/v2/name/" + country + "?fullText=true");
-       let getFlag = response.data[0].flag;
-       const flagImage = document.createElement("img");
-       flagImage.setAttribute("src", getFlag);
-       const countryInfo = document.createElement("p");
-       countryInfo.setAttribute('style', 'white-space: pre;');
-       countryInfo.textContent = response.data[0].name + "\n" + response.data[0].name + " is situated in " + response.data[0].subregion + ". \n" +
-           "It has a population of " + response.data[0].population + " people. \n" +
-           "The capital city is " + response.data[0].capital + " \n" +
-           getCurrencyInformation(response.data) + "\n" +
-           getLanguageInformation(response.data);
-       container.appendChild(flagImage);
-       container.appendChild(countryInfo);
-   } catch (e){
-            const errormessage = document.createElement("p")
-            errormessage.textContent = 'dat is echt geen land'
-            container.appendChild(errormessage)
-   }
+    const loadingElement = document.createElement('img')
+    loadingElement.src = 'BeneficialTepidFieldspaniel-max-1mb.gif'
+    container.appendChild(loadingElement)
+    try {
+        const response = await axios.get("https://restcountries.eu/rest/v2/name/" + country + "?fullText=true");
+        let getFlag = response.data[0].flag;
+        const flagImage = document.createElement("img");
+        flagImage.setAttribute("src", getFlag);
+        const countryInfo = document.createElement("p");
+        countryInfo.setAttribute('style', 'white-space: pre;');
+        countryInfo.textContent = response.data[0].name + "\n" + response.data[0].name + " is situated in " + response.data[0].subregion + ". \n" +
+            "It has a population of " + response.data[0].population + " people. \n" +
+            "The capital city is " + response.data[0].capital + " \n" +
+            getCurrencyInformation(response.data) + "\n" +
+            getLanguageInformation(response.data);
+        container.appendChild(flagImage);
+        container.appendChild(countryInfo);
+    } catch (e) {
+        const errormessage = document.createElement("p");
+        if (country === ''){
+            errormessage.textContent = 'Je typte niets';
+        } else {
+            errormessage.textContent = 'Dat is geen land, heb je misschien een typfout gemaakt? Je typte: ' + country;
+        }
+        errormessage.style.color = 'red';
+        errormessage.style.backgroundColor = 'yellow';
+        errormessage.style.borderRadius = '12px'
+        errormessage.style.padding = '5px'
+        container.appendChild(errormessage);
+    }
+    container.removeChild(loadingElement)
 }
 
 function getLanguageInformation(country) {
@@ -42,9 +54,9 @@ function getLanguageInformation(country) {
     }
 }
 
-function getCurrencyInformation(country) { //als alles werkt, bug wegwerken voor valuta eindigend op medeklinkers (vb. gulden's moet zijn guldens)
+function getCurrencyInformation(country) {
     if (country[0].currencies.length < 2) {
-        return "and you can pay with " + country[0].currencies[0].name + "'s";
+        return "and you can pay with " + country[0].currencies[0].name + "s";
     }
     if (country[0].currencies.length >= 2) {
         return "and you can pay with " + country[0].currencies[0].name + "'s and " + country[0].currencies[1].name;
@@ -66,7 +78,6 @@ countryInput.addEventListener('keypress', function (e) {
         removeChildren();
         const userInput = countryInput.value;
         getCountryInformation(userInput);
-        console.log(userInput);
         countryInput.value = "";
     }
 });
