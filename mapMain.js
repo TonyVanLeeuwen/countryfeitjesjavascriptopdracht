25f836b44cbbeac2f0c4e55229dd92ba2f7d49e2
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-03-07 12:57:16 +0100

    vlaggen toegevoegd, listelementen aan de dom toegevoegd

--- mapMain.js
+++ mapMain.js
@@ -1,9 +1,28 @@
+const countryContainer = document.getElementsByClassName("countryMapContainer");
+const countryList = document.getElementById("countrylist");
 
-console.log("gekoppeld?")
 
-async function getAllCountries(){
-    const allCountriesResponse = await axios.get("https://restcountries.eu/rest/v2/all")
+async function getAllCountries() {
+    const allCountriesResponse = await axios.get("https://restcountries.eu/rest/v2/all");
+    allCountriesResponse.data.sort((countryA, countryB) => {
+        return countryA.population - countryB.population;
+    });
+    for (const country of allCountriesResponse.data) {
+        addCountryToList(country);
+    }
     return allCountriesResponse;
 }
 
-console.log(getAllCountries())
+function addCountryToList(country) {
+    const {name, flag} = country;
+    const countryElement = document.createElement('li');
+    countryElement.textContent = name;
+    const flagElement = document.createElement('img');
+    flagElement.setAttribute('src', flag);
+    flagElement.setAttribute('class', 'flag');
+    countryElement.appendChild(flagElement);
+    countryList.appendChild(countryElement);
+}
+
+
+getAllCountries();
