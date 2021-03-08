04bd16ca35286dde525c0bed14b3f41d985f1adb
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-03-08 14:17:03 +0100

    kleuren van landen aangepast door middel van een switch statement en CSS in JS

--- mapMain.js
+++ mapMain.js
@@ -4,25 +4,62 @@ const countryList = document.getElementById("countrylist");
 
 async function getAllCountries() {
     const allCountriesResponse = await axios.get("https://restcountries.eu/rest/v2/all");
-    allCountriesResponse.data.sort((countryA, countryB) => {
-        return countryA.population - countryB.population;
-    });
+    sortCountries(allCountriesResponse);
     for (const country of allCountriesResponse.data) {
         addCountryToList(country);
+    }
+    for (const country of allCountriesResponse.data){
+        checkContinentForCountry(country)
     }
     return allCountriesResponse;
 }
 
-function addCountryToList(country) {
+async function addCountryToList(country) {
+    //class toevoegen aan countryelement
     const {name, flag} = country;
     const countryElement = document.createElement('li');
     countryElement.textContent = name;
     const flagElement = document.createElement('img');
     flagElement.setAttribute('src', flag);
     flagElement.setAttribute('class', 'flag');
+
+    switch (checkContinentForCountry(country)){
+        case "Africa":
+            countryElement.style.color = "rgb(91,118,188)"
+            break;
+        case "Americas":
+            countryElement.style.color = "rgb(89,128,81)"
+            break;
+        case "Asia":
+            countryElement.style.color = "rgb(194,87,94)"
+            break;
+        case "Europe":
+            countryElement.style.color = "rgb(249,212,88)"
+            break;
+        case "Oceania":
+            countryElement.style.color = "rgb(155,90,181)"
+            break;
+        default:
+            countryElement.style.color = "pink"
+            break;
+    }
+
     countryElement.appendChild(flagElement);
     countryList.appendChild(countryElement);
+
 }
+
+function checkContinentForCountry(country){
+        const {region} = country;
+        return region;
+}
+
+function sortCountries(countries) {
+    countries.data.sort((countryA, countryB) => {
+        return countryA.population - countryB.population;
+    });
+}
+
 
 
 getAllCountries();
--- pseudo2.md
+++ pseudo2.md
@@ -2,10 +2,12 @@ Setup tweede pagina + JS
 
 Haal alle landen op door middel van Axios.get (kijken of er een optie is om alles binnen te halen) [x]
 Sorteer de landen van laag naar hoog op basis van populatie (array sort) [x]
-plaats een lijst in de DOM met de vlaggetjes als de puntjes (UL) []
+plaats een lijst in de DOM met de vlaggetjes (UL) [x]
 Landkaart moet bovenaan de pagina []
-Schrijf een functie die checkt bij welk continent welk land hoort []
-gebruik de functie om icm JS CSS kleur te geven aan ieder land []
+Schrijf een functie die checkt bij welk continent welk land hoort [x]
+gebruik de functie om icm JS CSS kleur te geven aan ieder land [x]
+functie toevoegen om de populatie aan de dom toe te voegen [x]
+eventlistener toevoegen die bovenstaande functie aanroept bij een klik op het land [x]
 Voeg een link toe aan de hoofdpagina en aan de map pagina om de gebruiker heen en terug te sturen []
 
 Bonus:
@@ -13,4 +15,7 @@ zoek op hoe we een afbeelding het makkelijkst clickable kunnen maken []
 Doe dit eerst voor een enkel land (Rusland bijvoorbeeld) []
 Gebruik dit nu om de gebruiker de landkaart aan te laten klikken en een land te selecteren []
 
-¡prettify! []
+¡prettify!
+Maak de vlaggetjes netter op de pagina []
+Zorg dat de informatie goed getoond wordt[]
+
